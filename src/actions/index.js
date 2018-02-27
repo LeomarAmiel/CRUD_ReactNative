b297a6fe070ea5dec3b5eef2ef75ff7195340269
Leomar Alejar <leomaramieldev@gmail.com>
2018-02-27 17:07:19 +0800

    Add Add to firebase action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,28 @@
 import db from '../firebase';
 
-export const fetchData = () => (dispatch) => (
+export const fetchFromFirebase = () => (dispatch) => (
     db.collection('todos').doc('TMts7PEm7oyYJAMk0nVQ').get()
         .then((doc) => (
-            dispatch(getData(doc.data().todo))
+            dispatch(fetchData(doc.data().todo))
         ))
-)
+);
 
-export const getData = (payload) => ({
+export const addToFirebase = (todo) => (dispatch) => (
+    db.collection('todos').doc('TMts7PEm7oyYJAMk0nVQ').get()
+        .then((doc) => {
+            var newArr = doc.data().todo;
+            newArr.push(todo);
+            dispatch(addTodo(newArr));
+        })
+);
+
+export const fetchData = (payload) => ({
     type: "GET_TODO",
     payload
-})
+});
+
+
+export const addTodo = (payload) => ({
+    type: "ADD_TODO",
+    payload
+});
